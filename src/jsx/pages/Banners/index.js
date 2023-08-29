d612024df6ca02d249adfcd90f300e04f9395a26
Muhammed Osama <muhammed.o.nasser@gmail.com>
2023-08-29 13:22:00 +0300

    edit 29/8 part3

--- src/jsx/pages/Banners/index.js
+++ src/jsx/pages/Banners/index.js
@@ -8,25 +8,29 @@ import { toast } from "react-toastify";
 import ProductsService from "../../../services/ProductsService";
 import BaseService from "../../../services/BaseService";
 import BannerService from "../../../services/BannerService";
+import Loader from "../../common/Loader";
 
 const Banners = () =>{
     const [files, setFiles] = useState([{},{},{},{},{}])
     const [productsOptions, setProductsOptions] = useState([])
     const [isAdd, setIsAdd] = useState(true)
+    const [loading, setLoading] = useState(false)
+    const [submitLoading, setSumbitLoading] = useState(false)
     const Auth = useSelector(state=> state.auth?.auth)
     const isExist = (data)=> Auth?.admin?.admin_roles?.includes(data)
     const productsService = new ProductsService()
     const bannerService = new BannerService()
 
     const [formData, setFormData] = useState([
-        {src:'', product: ''},
-        {src:'', product: ''},
-        {src:'', product: ''},
-        {src:'', product: ''},
-        {src:'', product: ''},
+        {src:'', product: '', loading: false},
+        {src:'', product: '', loading: false},
+        {src:'', product: '', loading: false},
+        {src:'', product: '', loading: false},
+        {src:'', product: '', loading: false},
     ])
 
     useEffect(()=>{
+        setLoading(true)
         productsService?.getList()?.then(res=>{
             if(res.status === 200){
                 let products = res.data?.meta?.data?.map(prod=>{
@@ -53,7 +57,8 @@ const Banners = () =>{
                 let data = res?.data?.meta?.data?.map(item=>{
                     return {
                         src: item?.image,
-                        product: productsOptions?.filter(opt=> opt?.id === item.product_id)[0]
+                        product: !!item.product_id ? productsOptions?.filter(opt=> opt?.id === item.product_id)[0] : '',
+                        loading: false
                     }
                 })
 
@@ -67,10 +72,22 @@ const Banners = () =>{
                     setFormData([...data])
                 }
             }
+            setLoading(false)
         })
     },[productsOptions])
 
     const fileHandler = (e, index) => {
+        let updateImages = formData.map((item, ind)=>{
+            if(ind === index){
+                return {
+                    ...item,
+                    loading: true
+                }
+            } else {
+                return {...item}
+            }
+        } )
+        setFormData([...updateImages])
         let filesAll = e.target.files
         const filesData = Object.values(filesAll)
         let update = files?.map((file,updateIndex) => {
@@ -87,7 +104,8 @@ const Banners = () =>{
                     if(ind === index){
                         return {
                             ...item,
-                            src: res.data.url
+                            src: res.data.url,
+                            loading: false
                         }
                     } else {
                         return {...item}
@@ -101,36 +119,48 @@ const Banners = () =>{
 
     const onSubmit = () => {
         let data = {
-            banners: formData?.filter(res => !!res.src && !!res.product)?.map((item,index)=>{
-                return {
-                    image: item?.src, 
-                    product_id: item?.product?.value
+            banners: formData?.filter(res=> !!res.src)?.map((item,index)=>{
+                let res = {
+                    image: item?.src
                 }
+                if(!!item?.product?.value) res['product_id']=item?.product?.value
+                return res
             })
         }
         
         if(isAdd){
             if(data.banners?.length === 0){
-                toast.error('Add Image and Product First')
+                toast.error('Add Image')
                 return
-             }
+            }
+            setSumbitLoading(true)
             bannerService?.create(data)?.then(res=>{
                 if(res?.status === 201){
                     toast.success('Banners Added Successfully')
                     setIsAdd(false)
                 }
+                setSumbitLoading(false)
             })
         } else {
+            setSumbitLoading(true)
             bannerService?.update(data)?.then(res=>{
                 if(res?.status === 200){
                     toast.success('Banners Updated Successfully')
                     setIsAdd(false)
                 }
+                setSumbitLoading(false)
             })
         }
         
     }
 
+    if(loading){
+        return <Card className="py-5">
+            <Card.Body>
+                <Loader />
+            </Card.Body>
+        </Card>
+    }
     return(
         <>
         {formData?.map((data, index)=>{
@@ -153,6 +183,7 @@ const Banners = () =>{
                         </div>
                         <button 
                             className="delete"
+                            type="button"
                             onClick={()=>{
                                 let update= formData?.map((item, ind)=>{
                                     if(ind === index){
@@ -171,41 +202,17 @@ const Banners = () =>{
                         </button>
                         <div className="avatar-preview">
                             <div id={`imagePreview${index}`}>
-                            {!!data?.src && <img id={`saveImageFile${index}`} src={data?.src} alt='icon' />}
-                            {!data?.src && <img id={`saveImageFile${index}`} src={uploadImg} alt='icon'
+                            {(!!data?.src && !data.loading) && <img id={`saveImageFile${index}`} src={data?.src} alt='icon' />}
+                            {(!data?.src && !data.loading) && <img id={`saveImageFile${index}`} src={uploadImg} alt='icon'
                                 style={{
                                     width: '80px',
                                     height: '80px',
                                 }}
 							/>}
+                            {data.loading && <Loader />}
                             </div>
                         </div>
                     </div>
-                    {/* <div className="image-placeholder">	
-                        <div className="avatar-edit">
-                            <input type="file" 
-                                    onChange={(e) => {
-                                        if(!isExist('banners')){
-                                            toast.error('Not Allowed, Don`t have Permission')
-                                            return
-                                        }
-                                        fileHandler(e,index+1)
-                                    }} 
-                                    id={`imageUpload${index+1}`} /> 					
-                            <label htmlFor={`imageUpload${index+1}`}  name=''></label>
-                        </div>
-                        <div className="avatar-preview">
-                            <div id={`imagePreview${index+1}`}>
-                            {files[index]?.name && <img id={`saveImageFile${index+1}`} src={URL.createObjectURL(files[index])} alt='icon' />}
-                            {!files[index]?.name && <img id={`saveImageFile${index+1}`} src={uploadImg} alt='icon'
-                                style={{
-                                    width: '80px',
-                                    height: '80px',
-                                }}
-							/>}
-                            </div>
-                        </div>
-                    </div> */}
                     <div className='form-row mt-3'>
                         <div className='form-group w-100 d-flex align-items-center m-0'>
                             <label style={{width: '65px'}} className='m-0'>Link {index+1}:</label>
@@ -228,16 +235,36 @@ const Banners = () =>{
                                     setFormData([...updateFormData])
                                 }}
                             />
+                            {!!data?.product && <button 
+                                className="delete2"
+                                type="button"
+                                onClick={()=>{
+                                    let update= formData?.map((item, ind)=>{
+                                        if(ind === index){
+                                            return{
+                                                ...item,
+                                                product: ''
+                                            }
+                                        } else {
+                                            return item
+                                        }
+                                    })
+                                    setFormData([...update])
+                                }}
+                            >
+                            <i className="la la-trash text-danger"></i>
+                        </button>}
                         </div>
                     </div>
                     </Col>
                     </Row>
             </Card>
         })}
-        {isExist('banners') && <div className="d-flex justify-content-end">
+        {isExist('banners') && <div className="d-flex justify-content-end mb-4">
             <Button 
                 variant="primary" 
                 className="px-5"
+                disabled={submitLoading}
                 onClick={()=> onSubmit()}
             >Submit</Button>
         </div>}
